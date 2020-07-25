4ba47750a709acb46db2b7022a16edd7f48dfa9a
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-07-25 08:00:27 +0100

    Feature: user route added to app

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const userRouter = new express.Router();
+
+const urlencodedParser = bodyParser.urlencoded({
+  extended: false
+});
+
+userRouter.get('/', async (req, res) => {
+  res.render('home');
+});
+
+userRouter.post('/user', urlencodedParser, async (req, res) => {
+  res.send('register user')
+});
+
+module.exports = userRouter;
