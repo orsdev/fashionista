eaaad21b1ea2095f2d9bcc3d7f3bf511e62ed77c
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-07-25 08:51:09 +0100

    index.js modified

---
+++ index.js
@@ -0,0 +1,29 @@
+require('./src/db/mongoose');
+const express = require('express');
+const path = require('path');
+const adminRouter = require('./src/routes/admin');
+const userRouter = require('./src/routes/user');
+
+const app = express();
+const port = process.env.PORT;
+
+const staticPath = path.join(__dirname, 'public');
+const viewPath = path.join(__dirname, 'src', 'views', 'templates');
+
+app.set('view engine', 'hbs');
+app.set('views', viewPath);
+
+app.use(express.static(staticPath));
+
+// ROUTER MIDDLEWARES
+app.use('/admin', adminRouter);
+app.use(userRouter);
+
+// 404 PAGE MIDDLEWARE
+app.use('/', (req, res) => {
+  res.status(404).send('404 page');
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+}); 
