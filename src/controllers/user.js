f5f069f9732b9513e9b9b3e01b608f498873d249
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-07-29 20:27:37 +0100

    Refactor: getHomePage route controller removed from user.js

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,9 +1,3 @@
-const express = require('express');
-
-exports.getHomePage = async (req, res) => {
-  res.render('home', { pageTitle: 'FASHIONIT | HOME' });
-};
-
 exports.postUserInfo = async (req, res) => {
   res.send('register user')
 };
