d3e1c56a40b7542b6f34fb644a2063f39e6d39c4
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-08-01 12:44:41 +0100

    shop controller logic added

--- src/controllers/shop.js
+++ src/controllers/shop.js
@@ -1,9 +1,40 @@
+const ProductsClass = require("../model/products");
+
 exports.getHomePage = async (req, res) => {
-  res.render('home', { pageTitle: 'FASHIONIT | HOME' });
+
+  try {
+    const featuredProducts = ProductsClass.getFeaturedProducts(req, res, 5);
+    const homeProducts = ProductsClass.getHomeProducts(req, res, 12);
+
+    const mergedProducts = await Promise.all([featuredProducts, homeProducts]);
+
+    let featured = (!mergedProducts[0].length) ? 'No Featured Products' : mergedProducts[0];
+    let products = (!mergedProducts[1].length) ? 'No Products' : mergedProducts[1];
+
+    res.render('home', {
+      pageTitle: 'FASHIONIT | HOME',
+      featured,
+      products
+    });
+
+  } catch (e) {
+    res.status(400).send({ error: 'Bad Request' });
+  }
 };
 
 exports.getAllProducts = async (req, res) => {
-  res.render('shop/shop', { pageTitle: 'FASHIONIT | SHOP' });
+
+  try {
+    const allProducts = await ProductsClass.getAllProducts(req, res, 20);
+    let products = (!allProducts.length) ? 'No Products' : allProducts;
+
+    res.render('shop/shop', {
+      pageTitle: 'FASHIONIT | SHOP',
+      products
+    });
+  } catch (e) {
+    res.status(400).send({ error: 'Bad Request' });
+  }
 };
 
 exports.getCart = async (req, res) => {
