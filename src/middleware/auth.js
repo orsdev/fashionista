7c4e6c90adf09bc1bfceca6f85438ece6211b5b7
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-08-30 06:42:39 +0100

    auth middleware modified

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,11 +4,15 @@ const isAuth = (req, res, next) => {
   if (!(req.session.isAuthenticated && req.session.user)) {
     return res.redirect('/login');
   }
-  const user = User.findById(req.session.user._id);
-  user.then((response) => {
-    req.user = response;
-    return next();
-  }).catch(() => res.redirect('/login'));
+
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
+        return res.redirect('/login');
+      }
+      req.user = user;
+      return next();
+    }).catch(() => res.redirect('/login'));
 
 };
 
