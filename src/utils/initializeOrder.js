e72ac723b80483f59d17c72a8424878af7b4461c
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-10-03 05:57:05 +0100

    initializeOrder util function added to app

---
+++ src/utils/initializeOrder.js
@@ -0,0 +1,35 @@
+const { Order } = require('../model/order');
+
+const initializeOrder = async (req, res, next) => {
+  if (req.session.user && req.session.isAuthenticated) {
+    try {
+      const userOrder = await Order.find({ 'user.userId': req.session.user._id });
+      if (userOrder.length == 0) {
+
+        const cartOrder = {
+          order: [],
+          user: {
+            userName: req.session.user.fullName,
+            userId: req.session.user._id
+          }
+        }
+
+        const productOrder = new Order(cartOrder);
+        productOrder.save()
+          .then((response) => {
+            return next();
+          }).catch((e) => {
+            return next(e);
+          })
+      } else {
+        return next();
+      }
+    } catch (e) {
+      return next(e);
+    }
+  } else {
+    return next();
+  }
+}
+
+module.exports = initializeOrder;
