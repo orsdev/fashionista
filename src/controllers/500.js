69c6ce77925cc926520a6ef1af2b625ea167becc
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-10-14 18:07:02 +0100

    minor changes made to app

--- src/controllers/500.js
+++ src/controllers/500.js
@@ -1,6 +1,7 @@
 exports.get500Page = (error, req, res, next) => {
   res.status(500);
   res.render('500', {
+    path: req.originalUrl,
     pageTitle: 'FASHIONIT | SERVER ERROR',
     errorMessage: error
   });
