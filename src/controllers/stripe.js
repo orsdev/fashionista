5ee62b730ae261a21d01f2d4ae49a4e6e615c8e3
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-10-14 10:28:37 +0100

    stripe controller & route refactored

--- src/controllers/stripe.js
+++ src/controllers/stripe.js
@@ -1,4 +1,5 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const flashError = require('../utils/flashError');
 const orderSuccessMessage = require('../mail/orderSuccessMessage');
 const adminUserOrderSummary = require('../mail/adminUserOrder');
 
@@ -10,12 +11,12 @@ exports.getSuccessPage = async (req, res, next) => {
   // Redirect to order if session_id is missing
   if (!session_id) {
     return res.redirect('/order');
-  };
+  }
 
   // Redirect to shop if order is empty
   if (!order.length) {
     return res.redirect('/shop');
-  };
+  }
 
   try {
     const session = await stripe.checkout.sessions.retrieve(session_id);
@@ -23,21 +24,18 @@ exports.getSuccessPage = async (req, res, next) => {
       expand: ['payment_method'],
     });
 
-    const getBillingAddress = customer.payment_method.billing_details.address
+    const getBillingAddress = customer.payment_method.billing_details.address;
     const { line1, line2 } = getBillingAddress;
-    let getAddress = line1 || line2;
+    const getAddress = line1 || line2;
 
     // Send Message to user
     orderSuccessMessage(order, userEmail, fullName.split(' ')[0]);
-
     // Send Order Summary to admin
     adminUserOrderSummary(order, fullName.split(' ')[0], getAddress);
 
     // Clear products in order cart
     req.order.deleteOrder(req, res)
-      .then((response) => {
-        return res.render('stripe/success', { pageTitle: 'FASHIONIT | ORDER SUCCESS' })
-      })
+      .then((response) => res.render('stripe/success', { pageTitle: 'FASHIONIT | ORDER SUCCESS' }))
       .catch((error) => {
         const errMessage = 'Failed to clear order cart. Please try again.';
         return flashError(req, res, errMessage, '/order');
@@ -49,27 +47,27 @@ exports.getSuccessPage = async (req, res, next) => {
   }
 };
 
-exports.getCancelPage = (req, res, next) => {
-  return res.render('stripe/cancel', { pageTitle: 'FASHIONIT | ORDER Cancel' })
-}
+exports.getCancelPage = (req, res) => res.render('stripe/cancel', { pageTitle: 'FASHIONIT | ORDER CANCEL' });
 
 exports.createPayment = async (req, res) => {
 
   const orders = req.order.order.length && req.order.order;
-  let productData = [];
+  let productData;
   if (orders) {
-    for (const key of orders) {
-      productData.push({
+    productData = orders.reduce((accumulator, currentValue) => {
+      accumulator.push({
         price_data: {
           currency: 'usd',
           product_data: {
-            name: key.product.title
+            name: currentValue.product.title
           },
-          unit_amount: (key.product.price * 100)
+          unit_amount: (currentValue.product.price * 100)
         },
-        quantity: key.quantity
-      })
-    }
+        quantity: currentValue.quantity
+      });
+
+      return accumulator;
+    }, []);
   }
 
   try {
@@ -79,13 +77,13 @@ exports.createPayment = async (req, res) => {
       line_items: productData,
       billing_address_collection: 'required',
       mode: 'payment',
-      success_url: process.env.DOMAIN_URL + '/order-success?session_id={CHECKOUT_SESSION_ID}',
-      cancel_url: process.env.DOMAIN_URL + '/order-cancel'
+      success_url: `${process.env.DOMAIN_URL}/order-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.DOMAIN_URL}/order-cancel`
     });
 
     res.json({ id: session.id });
   } catch (e) {
-    res.json({ error: 'Error: An error occurred with your connection to Stripe' })
+    res.json({ error: 'Error: An error occurred with your connection to Stripe' });
   }
 
 };
